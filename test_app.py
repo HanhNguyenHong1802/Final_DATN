d22211a2b18a4aa207c9dc32b6673e9db42690a1
HanhNguyenHong1802 <hanhnguyee2000@gmail.com>
2022-06-07 15:21:45 +0700

    control spotify

---
+++ test_app.py
@@ -0,0 +1,11 @@
+from pynput import mouse, keyboard
+from pynput.keyboard import Key
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import time
+
+from keras.models import load_model,Model
+import control_commands
